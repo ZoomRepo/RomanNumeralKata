72a9f28cc34285dd074ecb0dde34898184d5cdcd
ZoomRepo <ollybeumkes@hotmail.co.uk>
2022-03-23 20:51:31 +0000

    calculator return I given 11

--- RomanNumeralKata.Application/Calculator.cs
+++
@@ -1,8 +0,0 @@
-﻿/*
-
-*/
-Console.WriteLine("Roman Numeral Converter\n");
-Console.WriteLine("Enter a Roman Numeral to convert: ");
-string numeral = Console.ReadLine();
-
-// Implement method to calculate Roman Numeral's Numeric value.
